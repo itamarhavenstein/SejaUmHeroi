6f976fe7be67892bc8e616a2e4758c8e48c8e34e
ItamarHavenstein <itamarhavenstein@hotmail.com>
2020-12-15 20:54:06 -0300

    utilização das rotas

--- mobile/App.js
+++ mobile/App.js
@@ -1,13 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 import Routes from './src/routes';
 export default function App() {
   return (
-    <View>
-      <Text>Já vai </Text>
-      <StatusBar style="auto" />
-    </View>
+   <Routes />
   );
 }
